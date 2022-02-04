7951868cbdc2929b2aed68dbf37cc07c4714ba17
CalebHatch <calebhatchcontact@gmail.com>
2022-02-04 16:04:54 -0600

    Created initial project

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // multiply 1 by 2.2 and put it in a for loop
+        System.out.println("Hello world");
+	// write your code here
+    }
+}
