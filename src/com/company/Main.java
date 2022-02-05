c7676991d1593fef5d2342e54dd86cfe50bc2ab9
CalebHatch <calebhatchcontact@gmail.com>
2022-02-04 19:18:40 -0600

    Finished converter table

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,38 @@
+/** Week 5, Program 5.1
+ * by Caleb Hatch
+ */
+
 package com.company;
+
+
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // multiply 1 by 2.2 and put it in a for loop
-        System.out.println("Hello world");
-	// write your code here
+        public static void main(String[] args) {
+
+            // multiply 1 by 2.2 and put it in a for loop
+
+            int ar[] = {1, 3, 5, 7, 9, 11, 13, 15};
+            int i, kg;
+            double lb;
+
+            System.out.printf("%-10s %10s", "Kilograms", "Pounds\n");
+            System.out.println("---------     ------\s");
+
+            for (i = 0; i < ar.length; i++) {
+                kg = ar[i];
+                lb = (ar[i] * 2.2);
+
+                BigDecimal bd = new BigDecimal(lb);
+                bd = bd.round(new MathContext(2));
+                double lbRounded = bd.doubleValue();
+
+
+                System.out.printf("%-10s", kg);
+                System.out.printf("%10s\n", lbRounded);
+            }
+        }
     }
-}
+
